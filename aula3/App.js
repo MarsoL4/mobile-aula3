172b4f7ba348664e6abcca7ceac1529798f844d8
Marsol4 <enzogmarsola@gmail.com>
2025-02-24 09:38:41 -0300

    cria variáveis de estilo para ambas as views da página inicial

--- aula3/App.js
+++ aula3/App.js
@@ -4,34 +4,40 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{
-        alignSelf: 'stretch',
-        flex: 5,
-        backgroundColor: '#f00',
-        alignItems: 'center',
-        justifyContent: 'center'
-        }}>
+      <View style={viewComStyle}>
         <Text>Bom dia!</Text>
       </View>
 
-      <View style={{
-        flex: 5,
-        alignSelf: 'stretch', 
-        backgroundColor: 'skyblue',
-        alignItems: 'center',
-        justifyContent: 'center'
-        }}>
+      <View style={view2ComStyle}>
         <Text>Turma Fiap!</Text>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const viewComStyle ={
+  alignSelf: 'stretch', //Ocupe todo os espaço
+  flex: 5, // seja 50% da tela
+  backgroundColor: '#f00', //mudando a cor
+  alignItems: 'center', //alinhando os itens
+  justifyContent: 'center' // centralizando o conteúdo
+}
+
+const view2ComStyle = {
+  flex: '5',
+  alignSelf: 'stretch',
+  backgroundColor: 'skyblue',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
+const meuStyle = {
   container: {
     flex: 1,
     backgroundColor: '#ff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}
+
+const styles = StyleSheet.create(meuStyle);
